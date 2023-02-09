78148b1dfbe5aa8ff0f3f382b9afc6b645274b5b
Matheus Simão Caixeta <101995486+MatheusSC017@users.noreply.github.com>
2023-02-09 20:35:02 -0300

    Models configuration and migrations

--- store/cart/models.py
+++ store/cart/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.contrib.auth.models import User
+from product.models import Product
 
-# Create your models here.
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, verbose_name="usuário")
+    total = models.FloatField(validators=[MinValueValidator(0)], verbose_name="total")
+    subtotal = models.FloatField(validators=[MinValueValidator(0)], verbose_name="sub-total")
+    freight = models.FloatField(verbose_name="frete")
+
+    def __str__(self):
+        return f'{self.user} - R$ {self.total:.2f}'
+
+    class Meta:
+        verbose_name = 'carrinho'
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.DO_NOTHING, verbose_name="carrinho")
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING, verbose_name="produto")
+    unitary_value = models.FloatField(validators=[MinValueValidator(0)], verbose_name="valor unitário")
+    quantity = models.PositiveSmallIntegerField(verbose_name="quantidade")
+
+    def __str__(self):
+        return f'{self.cart} - R$ {self.unitary_value * self.quantity:.2f}'
+
+    class Meta:
+        verbose_name = 'item do carrinho'
--- store/product/models.py
+++ store/product/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name="produto")
+    describe = models.TextField(verbose_name="descrição")
+    price = models.FloatField(validators=[MinValueValidator(0)], verbose_name="preço")
+    stock = models.PositiveSmallIntegerField(verbose_name="estoque")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'produto'
