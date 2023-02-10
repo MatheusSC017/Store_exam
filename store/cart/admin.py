a0da9d4dcea4648ad826743a828fe6ffb41d744d
Matheus Simão Caixeta <101995486+MatheusSC017@users.noreply.github.com>
2023-02-09 21:23:12 -0300

    Configuração da área administrativa do sistema

--- store/cart/admin.py
+++ store/cart/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Cart, CartItem
 
-# Register your models here.
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'user', 'total', 'subtotal', 'freight', ]
+    list_display_links = ['pk', 'user', ]
+    search_fields = ['user', ]
+    list_per_page = 50
+
+
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'cart', 'product', 'unitary_value', 'quantity', ]
+    list_display_links = ['pk', 'cart', 'product', ]
+    search_fields = ['cart', 'product', ]
+    list_per_page = 50
+
+
+admin.site.register(Cart, CartAdmin)
+admin.site.register(CartItem, CartItemAdmin)
--- store/product/admin.py
+++ store/product/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'price', 'stock', ]
+    list_display_links = ['pk', 'name', ]
+    search_fields = ['name', ]
+    list_per_page = 50
+
+
+admin.site.register(Product, ProductAdmin)
